b08c333b22ef23aadbb95f600242f832dbb66280
wscjxky <619677632@qq.com>
2018-09-21 17:02:39 +0800

    init

--- python_worm/dir_tree.py
+++ python_worm/dir_tree.py
@@ -1,6 +1,6 @@
 import os
 
-import pymongo
+# import pymongo
 import xlrd
 import xlutils.copy
 import xlwt
@@ -9,10 +9,11 @@ import xlsxwriter
 from python_worm.common_function import clear_char
 
 xls_file='../cfg/list.xlsx'
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-mydb = myclient["runoobdb"]
+# myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+# mydb = myclient["runoobdb"]
 wb = xlrd.open_workbook(xls_file)
 sheet = wb.sheet_by_index(0)
+a_set=set()
 for irow in range(sheet.nrows):
     c_row = sheet.row(irow)
     A = c_row[0].value
@@ -23,19 +24,34 @@ for irow in range(sheet.nrows):
     B=clear_char(B)
     C=clear_char(C)
     D=clear_char(D)
-    dir=A
-    if not os.path.isdir('../Data/环球军事网/'+dir):
-        os.mkdir('../Data/环球军事网/'+dir)
-    dir = A + '/' + B
-    if not os.path.isdir('../Data/环球军事网/' + dir):
-        os.mkdir('../Data/环球军事网/' + dir)
-    dir = A + '/' + B
-    if not os.path.isdir('../Data/环球军事网/' + dir):
-        os.mkdir('../Data/环球军事网/' + dir)
-    dir = A + '/' + B + '/' + C
-    if not os.path.isdir('../Data/环球军事网/' + dir):
-        os.mkdir('../Data/环球军事网/' + dir)
-    dir = A + '/' + B + '/' + C +'/' + D
-    if not os.path.isdir('../Data/环球军事网/' + dir):
-        os.mkdir('../Data/环球军事网/' + dir)
-    print(A,B,C,D)
+    a_set.add(B)
+print(a_set)
+
+
+wbk=xlwt.Workbook()
+sheet1=wbk.add_sheet('0')
+
+for i,j in enumerate(a_set):
+    sheet1.write(i,0,str(i))
+    sheet1.write(i,1,j)
+
+wbk.save("yolo_category.xlsx")
+
+
+
+    # dir=A
+    # if not os.path.isdir('../Data/环球军事网/'+dir):
+    #     os.mkdir('../Data/环球军事网/'+dir)
+    # dir = A + '/' + B
+    # if not os.path.isdir('../Data/环球军事网/' + dir):
+    #     os.mkdir('../Data/环球军事网/' + dir)
+    # dir = A + '/' + B
+    # if not os.path.isdir('../Data/环球军事网/' + dir):
+    #     os.mkdir('../Data/环球军事网/' + dir)
+    # dir = A + '/' + B + '/' + C
+    # if not os.path.isdir('../Data/环球军事网/' + dir):
+    #     os.mkdir('../Data/环球军事网/' + dir)
+    # dir = A + '/' + B + '/' + C +'/' + D
+    # if not os.path.isdir('../Data/环球军事网/' + dir):
+    #     os.mkdir('../Data/环球军事网/' + dir)
+    # print(A,B,C,D)
